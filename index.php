0111b70247b9eab9855dc4f34f7557a6bbaa0d5a
Cathy Tasama <ctasama@cnm.edu>
2020-03-10 14:23:36 -0600

    created data design page and linked the persona, user story and user interaction flow pages to it.

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html langa="en">
+<head>
+	<meta charset="UTF-8" />
+	<title>Data Design</title>
+	<a href="epic/persona.php">Persona</a>
+	<a href="epic/user-story.php">User Story</a>
+	<a href="epic/user-case-interaction-flow.php">User Interaction Flow</a>
+</head>
+<body>
+<p>Data Design</p>
+</body>
+</html>
