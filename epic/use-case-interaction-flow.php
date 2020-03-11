69abde71a0df870cad4377c432bad8c23bb8c6b7
Cathy Tasama <ctasama@cnm.edu>
2020-03-11 16:07:10 -0600

    personalized the use case interaction story

---
+++ epic/use-case-interaction-flow.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+<html langa="en">
+<head>
+	<meta charset="UTF-8" />
+	<title>Use Case Interaction Flow</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<h1>Use Case Interaction Flow</h1>
+<div class="center">
+<p>Nah just closed from work and she is on her way home, It was a stressful day, She need to listen to her favorite playlist</p>
+	<ul>Precondition: Nah should be logged into her spotify account</ul>
+	<ul>
+		<li>While logged into her spotify account, she clicks on her library</li>
+		<li>She is presented with the playlists she had previously created</li>
+		<li>She chooses the playlist she wants to listen to</li>
+		<li>She is presented with the option to play the chosen playlist</li>
+		<li>She clicks play</li>
+	</ul>
+	</div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -5,7 +5,8 @@
 	<title>Data Design</title>
 	<a href="epic/persona.php">Persona</a>
 	<a href="epic/user-story.php">User Story</a>
-	<a href="epic/user-case-interaction-flow.php">User Interaction Flow</a>
+	<a href="epic/use-case-interaction-flow.php">Use Interaction Flow</a>
+	<a href="epic/edr.php">EDR</a>
 </head>
 <body>
 <p>This page includes the persona, user story and user interaction flow of a spotify user called Nah Blessings</p>
